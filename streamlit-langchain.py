f19db12a1794c6be929ad9b614d958abd65672c7
GediminasVaicaitis <gedasltu22@gmail.com>
2025-07-03 19:39:58 +0300

    added

---
+++ streamlit-langchain.py
@@ -0,0 +1,75 @@
+#Chatbot using LangChain and Streamlit
+# This code demonstrates how to create a simple chatbot using LangChain and Streamlit.
+# Run : streamlit run streamlit-langchain.py 
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.vectorstores import InMemoryVectorStore
+import os
+from langchain import hub
+from dotenv import load_dotenv
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.document_loaders import WebBaseLoader
+import bs4  # BeautifulSoup for parsing HTML
+
+load_dotenv()  # take environment variables
+
+os.environ["USER_AGENT"] = "Mozilla/5.0 (compatible; MyStreamlitBot/1.0; +https://yourdomain.com/bot)"
+
+# from .env file
+# Load environment variables from .env file
+
+token = os.getenv("SECRET_OPENAI")  # Replace with your actual token
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-4.1-nano"
+
+loader = WebBaseLoader(
+    web_paths=("https://w.wiki/EXa6",),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+splits = text_splitter.split_documents(docs)
+
+embeddings=OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    base_url="https://models.inference.ai.azure.com",
+    api_key=token, # type: ignore
+)
+
+vectorstore = InMemoryVectorStore(embeddings)
+
+_ = vectorstore.add_documents(documents=splits)
+
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    print(docs)
+    return "\n\n".join(doc.page_content for doc in docs)
+
+st.title("Streamlit LangChain Chatbot: about Kaunas")
+
+def generate_response(input_text):
+    llm = ChatOpenAI(base_url=endpoint, temperature=0.7, api_key=token, model=model)
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+        )
+    
+    
+    st.info(rag_chain.invoke(input_text))
+
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter text:",
+        placeholder="Type your question here...",)
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        generate_response(text)
